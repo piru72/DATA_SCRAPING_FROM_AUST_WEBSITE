36e307175c3ee92252a8fe5cedad639707aa262c
piru72 <parvezdirom2000@gmail.com>
2022-12-08 23:23:20 +0600

    data scrapped

--- Teacher_Scrapper/teachers_info_scrapper.js
+++ Teacher_Scrapper/teachers_info_scrapper.js
@@ -2,31 +2,41 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const writeStream = fs.createWriteStream('teachers_info.csv');
+const departmentName = 'te'
+const fileName =  'teachers_info_' + departmentName + '.csv';
+const WebsiteLink = 'https://www.aust.edu/' + departmentName + '/faculty_members'
+const writeStream = fs.createWriteStream(fileName);
 
-writeStream.write(`Name,Designation,email,imaage_link\n`);
 
-request('https://www.aust.edu/eee/faculty_members', (error, response, html) => {
+writeStream.write(`Name,Designation,Email,Imagelink,Phone Number\n`);
+
+
+request( WebsiteLink, (error, response, html) => {
 
     if (!error && response.statusCode == 200) {
         console.log('Successfully scraped the website.');
         const $ = cheerio.load(html);
         $('.card').each((i, el) => {
-         
 
-            const Designation = $(el).find('.card-text').text();
             const name = $(el).find('.card-title').text();
-            const email = $(el).find('.fac_email').text();
+            const designation = $(el).find('.card-text').text().trim();
+
+            var email = "Not Available"
+            if ($(el).find('.fac_email').text().trim() != "")
+                email = $(el).find('.fac_email').text().trim();
+
             const img = $(el).children('img').attr('src');
-            
+            const phone = 'Not Available'
 
+            // console.log(name);
+            // console.log(designation);
+            // console.log(email)
+            // console.log(img)
+            // console.log(phone)
+            // console.log("\n")
 
-            console.log(name);
-            console.log(Designation);
-            console.log(email)
-            console.log(img)
-            console.log(" \n")
-            
+            writeStream.write(`${name},${designation},${email},${img},${phone}\n`);
+
         });
         console.log('Scraping Complete');
     }
